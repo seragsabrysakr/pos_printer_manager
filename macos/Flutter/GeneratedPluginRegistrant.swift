bf9c8f81d253d63bd4badbca744552d8ac78f945
F-BONAPARTA <67749770+bonaparta13@users.noreply.github.com>
2024-08-14 00:11:20 +0300

    update deps

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,8 @@
 import FlutterMacOS
 import Foundation
 
-import network_info_plus
 import printing
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  NetworkInfoPlusPlugin.register(with: registry.registrar(forPlugin: "NetworkInfoPlusPlugin"))
   PrintingPlugin.register(with: registry.registrar(forPlugin: "PrintingPlugin"))
 }
